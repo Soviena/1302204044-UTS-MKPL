e9215ced562d05c11d8072c7badf13564263ff7b
Soviena <rovino.rs@gmail.com>
2023-04-13 08:10:07 +0700

    simplified tax function

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -16,29 +16,18 @@ public class TaxFunction {
 	
 	
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
+		int taxMarried = 0;
 		int tax = 0;
 		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
+		if (numberOfMonthWorking > 12) System.err.println("More than 12 month working per year");
+		if (numberOfChildren > 3) numberOfChildren = 3;
+		if (isMarried) taxMarried = 4500000 + (numberOfChildren * 1500000);
 		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
+		tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + taxMarried)));
 		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
+		if (tax < 0) tax = 0;
+ 
+		return tax;
 	}
 	
 }
